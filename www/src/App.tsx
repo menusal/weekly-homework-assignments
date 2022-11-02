069f90784828877f42a5a6faef1ac85083ac8f6d
menusal <daniarlandis@gmail.com>
2022-11-02 08:09:52 +0100

    feat(home): add home actions wip

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,12 +1,28 @@
 function App() {
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">
-        Welcome!
-      </p>
-      <p className="text-gray-500 text-lg">
-        React and Tailwind CSS in action
-      </p>
+    <div className="mx-auto bg-slate-200 dark:bg-slate-800 p-8 h-screen">
+      <div className="container mx-auto bg-slate-50  dark:bg-slate-900 rounded-xl shadow p-8 m-1">
+        <p className="text-3xl text-gray-700 dark:text-white font-bold mb-5">
+          Welcome Noa
+        </p>
+        <p className="text-gray-500 dark:text-gray-300 text-lg">
+          Any homework to register?
+        </p>
+      </div>
+      <div className="container mx-auto bg-slate-50  dark:bg-slate-900 rounded-xl shadow p-8 m-8 text-center">
+        <button
+          type="button"
+          className=" w-full border border-blue-600 bg-blue-600 text-white rounded-md px-4 py-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
+        >Register homework</button>
+        <button
+          type="button"
+          className=" mt-4 w-full border border-green-600 bg-green-600 text-white rounded-md px-4 py-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
+        >Add new homework</button>
+         <button
+          type="button"
+          className=" mt-4 w-full border border-green-600 bg-green-600 text-white rounded-md px-4 py-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
+        >Show statics</button>
+      </div>
     </div>
   );
 }
